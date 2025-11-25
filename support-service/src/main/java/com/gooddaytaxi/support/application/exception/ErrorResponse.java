cf9f43893b7463bb2673fb91019b28afc6dc8ab2
Juyang_Jin <wndid2008@gmail.com>
2025-11-25 19:07:17 +0900

    setting : 패키지 구조 수정

---
+++ support-service/src/main/java/com/gooddaytaxi/support/application/exception/ErrorResponse.java
@@ -0,0 +1,20 @@
+package com.gooddaytaxi.support.application.exception;
+
+import com.gooddaytaxi.common.exception.ErrorCode;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+
+@Getter
+@AllArgsConstructor
+public class ErrorResponse {
+
+    private final String code;
+    private final String message;
+
+    public static ErrorResponse from(ErrorCode errorCode) {
+        return new ErrorResponse(
+                errorCode.getCode(),
+                errorCode.getMessage()
+        );
+    }
+}
