5ec2912a84cbe586a352ead7ada0e78fbce97cba
wlsql852@naver.com <wlsql852@naver.com>
2025-11-28 22:26:21 +0900

    refactor  - @EnableJpaAuditing을 config로 이동

--- payment-service/src/main/java/com/gooddaytaxi/payment/PaymentApplication.java
+++ payment-service/src/main/java/com/gooddaytaxi/payment/PaymentApplication.java
@@ -2,10 +2,8 @@ package com.gooddaytaxi.payment;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
 @SpringBootApplication
-@EnableJpaAuditing
 public class PaymentApplication {
 
     public static void main(String[] args) {
