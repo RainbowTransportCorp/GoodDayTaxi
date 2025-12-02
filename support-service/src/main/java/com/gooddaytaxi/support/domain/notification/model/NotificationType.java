777a976e52c279da0734ac70de5aeabb7ca20fb6
minjko <109601614+minjko@users.noreply.github.com>
2025-12-02 12:54:50 +0900

    Feat/support/#33 Notification, Log 엔티티 작성 (#47)
    * ✨ feat: Log, Notification Entity 및 도메인 로직 작성
    
    * ✨ feat: Log, Notification Entity 및 도메인 로직 작성
    
    * ✨ feat: Log, Notification Entity 및 도메인 로직 작성
    
    * ✨ feat: Log, Notification Entity 및 도메인 로직 작성

---
+++ support-service/src/main/java/com/gooddaytaxi/support/domain/notification/model/NotificationType.java
@@ -0,0 +1,18 @@
+package com.gooddaytaxi.support.domain.notification.model;
+
+/**
+ * 알림 타입 - 배차 할당과 수락, 운행 시작과 종료, 결제 완료, 장애 발생에 대한 알림
+ */
+public enum NotificationType {
+
+    DISPATCH_ASSIGNED, // 배차 할당 알림
+    DISPATCH_ACCEPTED, // 배차 수락 알림
+
+    TRIP_STARTED,      // 운행 시작 알림
+    TRIP_ENDED,        // 운행 종료 알림
+
+    PAYMENT_COMPLETED, // 결제 완료 알림
+
+    ERROR_DETECTED     // 장애 발생 알림
+}
+
