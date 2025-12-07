37ef858091bdbe719b3722468785eaa05a804dfa
wlsql852@naver.com <wlsql852@naver.com>
2025-12-07 22:50:42 +0900

    fix: 환불된, 취소된 경우가 아니면 새 결제 생성 불가로 설정

--- payment-service/src/main/java/com/gooddaytaxi/payment/domain/repository/PaymentRepository.java
+++ payment-service/src/main/java/com/gooddaytaxi/payment/domain/repository/PaymentRepository.java
@@ -4,6 +4,8 @@ import com.gooddaytaxi.payment.domain.entity.Payment;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 
 import java.time.LocalDateTime;
 import java.util.Optional;
@@ -12,4 +14,19 @@ import java.util.UUID;
 public interface PaymentRepository extends JpaRepository<Payment, UUID> , PaymentRepositoryCustom {
     Optional<Payment> findByTripId(UUID tripId);
     Page<Payment> searchPayments(String method, String status, UUID passeangerId, UUID driverId, UUID tripId, LocalDateTime startDay, LocalDateTime endDay, Pageable pageable);
+
+    @Query("""
+    SELECT CASE WHEN COUNT(p) > 0 THEN TRUE ELSE FALSE END
+    FROM Payment p
+    WHERE p.tripId = :tripId
+      AND p.status IN ('PENDING', 'IN_PROCESS', 'FAILED', 'COMPLETED')""")
+    boolean existByTripIdAndNotStatusForCreate(UUID tripId);
+
+    @Query("""
+    SELECT p
+    FROM Payment p
+    WHERE p.tripId = :tripId
+      AND p.status IN ('PENDING', 'IN_PROCESS', 'FAILED', 'COMPLETED')
+    ORDER BY p.createdAt DESC""")
+    Payment findLastByTripIdAndStatusForCreate(@Param("tripId") UUID tripId);
 }
