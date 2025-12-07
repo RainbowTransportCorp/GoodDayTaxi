30a65a4b14a95255e6d495519cf115edb2111081
이호준 <jake525@naver.com>
2025-12-07 23:23:05 +0900

    Feat : TripEvent 및 TripHistory 엔티티 추가

---
+++ trip-service/src/main/java/com/gooddaytaxi/trip/domain/model/enums/TripEventStatus.java
@@ -0,0 +1,7 @@
+package com.gooddaytaxi.trip.domain.model.enums;
+
+public enum TripEventStatus {
+    PENDING,
+    SENT,
+    FAILED
+}
---
+++ trip-service/src/main/java/com/gooddaytaxi/trip/domain/model/enums/TripEventType.java
@@ -0,0 +1,12 @@
+package com.gooddaytaxi.trip.domain.model.enums;
+
+public enum TripEventType {
+
+    STATUS_CHANGE,
+    LOCATION_UPDATE,
+    TRIP_CREATED,
+    TRIP_ASSIGNED,
+    TRIP_STARTED,
+    TRIP_ENDED,
+    TRIP_CANCELED
+}
