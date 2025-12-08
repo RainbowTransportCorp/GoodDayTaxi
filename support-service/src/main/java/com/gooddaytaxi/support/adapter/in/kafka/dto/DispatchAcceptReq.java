da045f5dede8c0398ffb3182ef257eb1a7e1ed21
minjko <komj36@gmail.com>
2025-12-08 12:10:33 +0900

    chore: moving files

--- support-service/src/main/java/com/gooddaytaxi/support/adapter/in/kafka/dto/DispatchAcceptReq.java
+++ support-service/src/main/java/com/gooddaytaxi/support/adapter/in/kafka/dto/DispatchAcceptReq.java
@@ -1,9 +1,5 @@
 package com.gooddaytaxi.support.adapter.in.kafka.dto;
 
-import lombok.AllArgsConstructor;
-import lombok.Getter;
-import lombok.NoArgsConstructor;
-
 /**
  * 배차 수락 DTO
  * 클라이언트로부터 수락된 배차 정보를 담는 객체
