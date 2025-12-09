f0cbaa24db0622ad106545e276fe5c0b2276e06c
이호준 <jake525@naver.com>
2025-12-09 14:54:03 +0900

    Feat : 기사 운행 이력 기능 구현

--- trip-service/src/main/java/com/gooddaytaxi/trip/infrastructure/adapter/TripQueryAdapter.java
+++ trip-service/src/main/java/com/gooddaytaxi/trip/infrastructure/adapter/TripQueryAdapter.java
@@ -1,22 +1,34 @@
 package com.gooddaytaxi.trip.infrastructure.adapter;
 
+import com.gooddaytaxi.trip.application.port.out.LoadTripsByDriverPort;
 import com.gooddaytaxi.trip.application.port.out.LoadTripsPort;
 import com.gooddaytaxi.trip.domain.model.Trip;
 import com.gooddaytaxi.trip.domain.repository.TripRepository;
 import com.gooddaytaxi.trip.infrastructure.persistence.TripJpaRepository;
 import lombok.RequiredArgsConstructor;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
 import org.springframework.stereotype.Component;
 
 import java.util.List;
+import java.util.UUID;
 
 @Component
 @RequiredArgsConstructor
-public class TripQueryAdapter implements LoadTripsPort {
+public class TripQueryAdapter implements LoadTripsPort, LoadTripsByDriverPort {
 
     private final TripJpaRepository tripJpaRepository;
 
     @Override
     public List<Trip> findAll() {     // ✅ findAll (L 하나)
         return tripJpaRepository.findAll();
+    }
+
+    @Override
+    public Page<Trip> loadTripsByDriverId(UUID driverId, int page, int size) {
+        return tripJpaRepository.findByDriverIdOrderByStartTimeDesc(
+                driverId,
+                PageRequest.of(page, size)
+        );
     }
 }
