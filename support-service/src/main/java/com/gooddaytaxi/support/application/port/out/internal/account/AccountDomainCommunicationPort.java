5fefc2622559ce0ebe9eb63f14fa7451201424ab
minjko <komj36@gmail.com>
2025-12-15 17:23:09 +0900

    feat: REFUND_REQUEST_CREATED, REFUND_REQUEST_REJECTED, REFUND_COMPLETED 기능 구현

--- support-service/src/main/java/com/gooddaytaxi/support/application/port/out/internal/account/AccountDomainCommunicationPort.java
+++ support-service/src/main/java/com/gooddaytaxi/support/application/port/out/internal/account/AccountDomainCommunicationPort.java
@@ -3,6 +3,7 @@ package com.gooddaytaxi.support.application.port.out.internal.account;
 import com.gooddaytaxi.support.adapter.out.internal.account.dto.DriverProfile;
 import com.gooddaytaxi.support.adapter.out.internal.account.dto.UserProfile;
 
+import java.util.List;
 import java.util.UUID;
 
 /* Account Domain과 통신하는 Port
@@ -11,4 +12,5 @@ import java.util.UUID;
 public interface AccountDomainCommunicationPort {
     UserProfile getUserInfo(UUID userId);
     DriverProfile getDriverInfo(UUID driverId);
+    List<UUID> getMasterAdminUuids();
 }
