0859abc1f1585f5d1a54274f83245b23331d485d
minjko <komj36@gmail.com>
2025-12-17 16:15:11 +0900

    feat: 관리자의 알림 전체 조회 - 필터링, 페이지네이션

--- support-service/src/main/java/com/gooddaytaxi/support/application/service/AdminNotificationFilter.java
+++ support-service/src/main/java/com/gooddaytaxi/support/application/service/AdminNotificationFilter.java
@@ -1,15 +1,9 @@
 package com.gooddaytaxi.support.application.service;
 
-import com.gooddaytaxi.support.domain.notification.model.NotificationType;
-
-import java.time.LocalDateTime;
-import java.util.UUID;
-
 public record AdminNotificationFilter(
         // 도메인
         String notificationOriginId,
         // 사용자
-        String notifierId,
         // 알림 타입
         String notificationType,
         // 날짜
