2ccb2925f5f3bcb6cce28b40f76cf90b60265da1
minjko <komj36@gmail.com>
2025-12-18 17:27:39 +0900

    feat: TRIP_LOCATION_UPDATED 이벤트 처리 구현

--- support-service/src/main/java/com/gooddaytaxi/support/application/port/in/trip/NotifyUpdatedLocationTripUsecase.java
+++ support-service/src/main/java/com/gooddaytaxi/support/application/port/in/trip/NotifyUpdatedLocationTripUsecase.java
@@ -1,7 +1,6 @@
 package com.gooddaytaxi.support.application.port.in.trip;
 
 import com.gooddaytaxi.support.application.dto.trip.TripLocationUpdatedCommand;
-import com.gooddaytaxi.support.application.dto.trip.TripStartedCommand;
 
 public interface NotifyUpdatedLocationTripUsecase {
     void execute(TripLocationUpdatedCommand command);
