762d738664ef2325886024a6ec5ac1526bb7c058
juyangjin <wndid2008@gmail.com>
2025-12-20 23:37:32 +0900

    docs: 주석 수정

--- dispatch-service/src/main/java/com/gooddaytaxi/dispatch/application/usecase/timeout/TripRequestMonitorScheduler.java
+++ dispatch-service/src/main/java/com/gooddaytaxi/dispatch/application/usecase/timeout/TripRequestMonitorScheduler.java
@@ -5,7 +5,7 @@ import lombok.RequiredArgsConstructor;
 import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.stereotype.Component;
 
-//@Component
+//@Component //에러 이벤트 컴포넌트는 아직 trip에서 반환 (ready) 이벤트를 발행하지 않으므로 주석처리
 @RequiredArgsConstructor
 public class TripRequestMonitorScheduler {
 
