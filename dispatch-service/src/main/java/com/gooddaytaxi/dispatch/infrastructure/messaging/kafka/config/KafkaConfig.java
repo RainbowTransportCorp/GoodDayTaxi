8513bc4e828e2215069ffa22ecd04153a25471e6
juyangjin <wndid2008@gmail.com>
2025-12-20 17:12:20 +0900

    refactor: 불필요한 producer용 Bean 제거

--- dispatch-service/src/main/java/com/gooddaytaxi/dispatch/infrastructure/messaging/kafka/config/KafkaConfig.java
+++ dispatch-service/src/main/java/com/gooddaytaxi/dispatch/infrastructure/messaging/kafka/config/KafkaConfig.java
@@ -1,9 +1,7 @@
 package com.gooddaytaxi.dispatch.infrastructure.messaging.kafka.config;
 
 import org.apache.kafka.clients.consumer.ConsumerConfig;
-import org.apache.kafka.clients.producer.ProducerConfig;
 import org.apache.kafka.common.serialization.StringDeserializer;
-import org.apache.kafka.common.serialization.StringSerializer;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
@@ -21,19 +19,6 @@ public class KafkaConfig {
     @Value("${spring.kafka.bootstrap-servers}")
     private String bootstrapServers;
 
-    @Bean
-    public ProducerFactory<String, String> producerFactory() {
-        Map<String, Object> props = new HashMap<>();
-        props.put(ProducerConfig.BOOTSTRAP_SERVERS_CONFIG, bootstrapServers);
-        props.put(ProducerConfig.KEY_SERIALIZER_CLASS_CONFIG, StringSerializer.class);
-        props.put(ProducerConfig.VALUE_SERIALIZER_CLASS_CONFIG, StringSerializer.class);
-        return new DefaultKafkaProducerFactory<>(props);
-    }
-
-    @Bean
-    public KafkaTemplate<String, String> kafkaTemplate() {
-        return new KafkaTemplate<>(producerFactory());
-    }
 
     @Bean
     public ConsumerFactory<String, String> consumerFactory(@Value("${spring.kafka.consumer.group-id:dispatch-group}") String groupId) {
