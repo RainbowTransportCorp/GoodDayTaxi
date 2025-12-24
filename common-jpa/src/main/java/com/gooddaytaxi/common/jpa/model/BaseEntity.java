ecd7d1e5f9a290bfd4d8f1cd777152a0385c1a39
minjko <komj36@gmail.com>
2025-12-24 22:09:31 +0900

    deploy: account 배포 준비

--- common-jpa/src/main/java/com/gooddaytaxi/common/jpa/model/BaseEntity.java
+++ common-jpa/src/main/java/com/gooddaytaxi/common/jpa/model/BaseEntity.java
@@ -4,7 +4,6 @@ import jakarta.persistence.Column;
 import jakarta.persistence.EntityListeners;
 import jakarta.persistence.MappedSuperclass;
 import lombok.Getter;
-import org.hibernate.annotations.Comment;
 import org.springframework.data.annotation.CreatedBy;
 import org.springframework.data.annotation.CreatedDate;
 import org.springframework.data.annotation.LastModifiedBy;
@@ -21,21 +20,17 @@ public abstract class BaseEntity {
 
     @CreatedDate
     @Column(name = "created_at", nullable = false, updatable = false)
-    @Comment("생성 시간")
     private LocalDateTime createdAt;
 
     @CreatedBy
     @Column(name = "created_by", updatable = false)
-    @Comment("생성자 ID")
     private UUID createdBy;
 
     @LastModifiedDate
     @Column(name = "updated_at")
-    @Comment("수정 시간")
     private LocalDateTime updatedAt;
 
     @LastModifiedBy
     @Column(name = "updated_by")
-    @Comment("수정자 ID")
     private UUID updatedBy;
 }
