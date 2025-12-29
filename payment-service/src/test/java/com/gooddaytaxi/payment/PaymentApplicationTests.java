1a27a2dd9ebaf1393cb20f239acff192efc639fb
juyangjin <wndid2008@gmail.com>
2025-12-29 16:13:11 +0900

    refactor: 서비스 로직 변경에 의한 테스트코드 주석처리

--- payment-service/src/test/java/com/gooddaytaxi/payment/PaymentApplicationTests.java
+++ payment-service/src/test/java/com/gooddaytaxi/payment/PaymentApplicationTests.java
@@ -1,15 +1,15 @@
-package com.gooddaytaxi.payment;
-
-import org.junit.jupiter.api.Disabled;
-import org.junit.jupiter.api.Test;
-import org.springframework.boot.test.context.SpringBootTest;
-
-@SpringBootTest
-class PaymentApplicationTests {
-
-    @Test
-    @Disabled("CI 환경에서 DB 연결 불필요")
-    void contextLoads() {
-    }
-
-}
+//package com.gooddaytaxi.payment;
+//
+//import org.junit.jupiter.api.Disabled;
+//import org.junit.jupiter.api.Test;
+//import org.springframework.boot.test.context.SpringBootTest;
+//
+//@SpringBootTest
+//class PaymentApplicationTests {
+//
+//    @Test
+//    @Disabled("CI 환경에서 DB 연결 불필요")
+//    void contextLoads() {
+//    }
+//
+//}
